6fe65ecadd2296570cd0901ab898bebfdda9eba1
sharonsyra <sharonwaithii@gmail.com>
2020-07-28 18:50:44 +0300

    DP-4260: Journal migration from postgres connection

--- src/main/scala/com/sharonsyra/migration/Main.scala
+++ src/main/scala/com/sharonsyra/migration/Main.scala
@@ -20,40 +20,52 @@ object Main extends App with ProtoEncryption {
       val rs = stm.executeQuery("SELECT * FROM journal")
       try {
         while (rs.next) {
-//          val person = (
-//            rs.getInt(1),
-//            rs.getString(2),
-//            rs.getInt(3),
-//            rs.getBoolean(4),
-//            rs.getString(5),
-//            encrypt(Any.pack(StateWrapper.parseFrom(rs.getBytes(6))))
-//          )
-//
-//          val prep = conn.prepareStatement("INSERT INTO new_journal VALUES (?, ?, ?, ?, ?, ?) ")
-//          prep.setInt(1, person._1)
-//          prep.setString(2, s"${person._2}")
-//          prep.setInt(3, person._3)
-//          prep.setBoolean(4, person._4)
-//          prep.setString(5, s"${person._5}")
-//          prep.setObject(6, person._6.toString.getBytes())
-//
-//          prep.executeUpdate
+          val person = (
+            rs.getInt(1),
+            rs.getString(2),
+            rs.getInt(3),
+            rs.getBoolean(4),
+            rs.getString(5),
+            encrypt(Any.pack(StateWrapper.parseFrom(rs.getBytes(6))))
+          )
+
+          val prep = conn.prepareStatement("INSERT INTO new_journal VALUES (?, ?, ?, ?, ?, ?) ")
+          prep.setInt(1, person._1)
+          prep.setString(2, s"${person._2}")
+          prep.setInt(3, person._3)
+          prep.setBoolean(4, person._4)
+          prep.setString(5, s"${person._5}")
+          prep.setObject(6, person._6.toString.getBytes())
+
+          prep.executeUpdate
+
         }
-
-
       }
       finally {
         rs.close()
       }
+
+      println("Migration starting...")
+
       stm.execute("ALTER TABLE journal RENAME TO old_journal")
       stm.execute("ALTER TABLE new_journal RENAME TO journal")
 
-      val last_ordering_number = stm.executeQuery("SELECT ordering FROM journal ORDER BY ordering DESC LIMIT 1")
-      println(last_ordering_number)
+      val last_ordering_number =
+        stm.executeQuery("SELECT ordering FROM journal ORDER BY ordering DESC LIMIT 1")
 
-      stm.execute("CREATE SEQUENCE new_ordering OWNED BY journal.ordering START $last_ordering_number INCREMENT 1")
+      while (last_ordering_number.next()) {
 
-      stm.execute("ALTER TABLE journal ALTER COLUMN ordering SET DEFAULT nextval(new_ordering)")
+        val next_ordering_number = last_ordering_number.getInt(1) + 1
+
+        stm.execute("ALTER TABLE journal " +
+          "ALTER ordering SET NOT NULL, " +
+          "ALTER ordering ADD GENERATED ALWAYS AS IDENTITY (START WITH "
+          + next_ordering_number + ");")
+
+        println("Migration is done")
+
+      }
+      last_ordering_number.close()
 
     }
     finally {
